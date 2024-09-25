04c52781a7e8ee8f170c0986065d947eef2d725f
jatnerubia <jasonlerit077@gmail.com>
2024-09-25 22:47:43 +0800

    feat: create basic layout for chat app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+const dummyData = Array.from(Array(40)).map((_, index) => `Message ${index}`);
 
 export default function Home() {
+  const [messages, setMessages] = useState<string[]>(dummyData);
+
   return (
-    <main>
-      <Button>Test</Button>
+    <main className="h-dvh">
+      <div className="h-full flex flex-col">
+        <div className="bg-slate-100 flex-1 flex flex-col gap-4 overflow-y-auto">
+          {messages.map((message, index) => (
+            <div key={index}>{message}</div>
+          ))}
+        </div>
+        <Button>Test</Button>
+      </div>
     </main>
   );
 }
