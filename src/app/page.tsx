c6621581678482b7c92732c077c202fe3b5f0280
jatnerubia <jasonlerit077@gmail.com>
2024-09-26 21:03:51 +0800

    update: add handleSubmit to stream chat api results

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,36 @@
 import { Button } from "@/components/ui/button";
 import { useState } from "react";
 
-const dummyData = Array.from(Array(40)).map((_, index) => `Message ${index}`);
-
 export default function Home() {
-  const [messages, setMessages] = useState<string[]>(dummyData);
+  const [messages, setMessages] = useState<string[]>([]);
+  const [message, setMessage] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: "What is A.I in one sentence?",
+      }),
+    });
+    if (!response.ok) {
+      throw new Error("Something went wrong");
+    }
+    if (response.body !== null) {
+      const reader = response.body.getReader();
+      while (true) {
+        const { value, done } = await reader.read();
+        if (done) break;
+        const chunk = new TextDecoder().decode(value);
+        setMessage((pv) => pv.concat(chunk));
+      }
+    }
+    setLoading(false);
+  };
 
   return (
     <main className="h-dvh">
@@ -15,8 +41,11 @@ export default function Home() {
           {messages.map((message, index) => (
             <div key={index}>{message}</div>
           ))}
+          {message}
         </div>
-        <Button>Test</Button>
+        <Button onClick={() => handleSubmit()} disabled={loading}>
+          Test
+        </Button>
       </div>
     </main>
   );
