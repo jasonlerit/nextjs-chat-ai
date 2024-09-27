c1fb8ec53fe369adf4330770a3ef3b58155452fb
jatnerubia <jasonlerit077@gmail.com>
2024-09-27 21:24:10 +0800

    update: add setLastMessage to useChatStore

--- src/stores/use-chat-store.ts
+++ src/stores/use-chat-store.ts
@@ -4,6 +4,7 @@ interface Chat {
   messages: string[]
   setMessages: (value: string[]) => void
   addMessage: (value: string) => void
+  setLastMessage: (value: string) => void
 }
 
 export const useChatStore = create<Chat>()((set) => ({
@@ -13,4 +14,16 @@ export const useChatStore = create<Chat>()((set) => ({
     set((state) => ({
       messages: [...state.messages, value],
     })),
+  setLastMessage: (value: string) =>
+    set((state) => {
+      const lastIndex = state.messages.length - 1
+      if (lastIndex < 0) {
+        return {
+          messages: [],
+        }
+      }
+      return {
+        messages: [...state.messages.slice(0, lastIndex), state.messages[lastIndex].concat(value)],
+      }
+    }),
 }))
