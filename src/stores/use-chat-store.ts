8bc12578984958d140f86fb662cb49e62d56f1a6
jatnerubia <jasonlerit077@gmail.com>
2024-09-27 20:56:05 +0800

    update: add addMessage to useChatStore

--- src/stores/use-chat-store.ts
+++ src/stores/use-chat-store.ts
@@ -3,9 +3,14 @@ import { create } from "zustand"
 interface Chat {
   messages: string[]
   setMessages: (value: string[]) => void
+  addMessage: (value: string) => void
 }
 
 export const useChatStore = create<Chat>()((set) => ({
   messages: [],
   setMessages: (value: string[]) => set({ messages: value }),
+  addMessage: (value: string) =>
+    set((state) => ({
+      messages: [...state.messages, value],
+    })),
 }))
