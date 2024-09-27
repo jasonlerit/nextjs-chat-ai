98c03851a93355db378ca8af6b393629e479d997
jatnerubia <jasonlerit077@gmail.com>
2024-09-27 21:25:54 +0800

    update: remove unsused state in chat list

--- src/app/components/chat-list.tsx
+++ src/app/components/chat-list.tsx
@@ -1,12 +1,9 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 "use client"
 
 import { useChatStore } from "@/stores/use-chat-store"
-import { useState } from "react"
 
 export const ChatList = () => {
   const messages = useChatStore((state) => state.messages)
-  const [message, setMessage] = useState<string>("")
 
   return (
     <div className='bg-slate-100 flex-1 flex flex-col gap-4 overflow-y-auto'>
@@ -15,7 +12,6 @@ export const ChatList = () => {
           <p className='whitespace-pre'>{message}</p>
         </div>
       ))}
-      <p className='whitespace-pre'>{message}</p>
     </div>
   )
 }
