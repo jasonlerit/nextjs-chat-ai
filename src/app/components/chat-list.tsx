292dd46be197785855ad2ed8c19f94673a2bf921
jatnerubia <jasonlerit077@gmail.com>
2024-09-28 10:24:56 +0800

    update: chat list styles

--- src/app/components/chat-list.tsx
+++ src/app/components/chat-list.tsx
@@ -2,17 +2,21 @@
 
 import { Card, CardContent } from "@/components/ui/card"
 import { useChatStore } from "@/stores/use-chat.store"
+import { Role } from "@/types/role.type"
 
 export const ChatList = () => {
   const messages = useChatStore((state) => state.messages)
 
   return (
     <div className='flex-1 overflow-y-auto'>
-      <div className='container mx-auto lg:max-w-4xl flex flex-col gap-4 px-4'>
+      <div className='container mx-auto lg:max-w-4xl flex flex-col gap-8 px-4'>
         {messages.map((message, index) => (
-          <Card key={index} className='w-11/12 lg:w-3/4'>
+          <Card
+            key={index}
+            className={`w-11/12 lg:w-3/4 ${message.role === Role.USER ? "self-end bg-primary text-primary-foreground" : "border-none shadow-none"}`}
+          >
             <CardContent className='p-4'>
-              <p className='whitespace-pre text-wrap'>{message}</p>
+              <p className='whitespace-pre text-wrap'>{message.content}</p>
             </CardContent>
           </Card>
         ))}
