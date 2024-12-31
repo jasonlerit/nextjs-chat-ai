cb15695609bca9eb6ea39f1b5b76f7657937a8bb
jatnerubia <jasonlerit077@gmail.com>
2024-12-31 08:24:52 +0800

    update: github link

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
         <span className='font-bold'>Chat AI</span>
         <div className='flex items-center gap-1'>
           <Button asChild variant='ghost' size='icon' aria-label='github link'>
-            <Link href='https://github.com/jatnerubia/nextjs-chat-ai'>
+            <Link href='https://github.com/jasonlerit/nextjs-chat-ai'>
               <GitHubLogoIcon className='w-5 h-5' />
             </Link>
           </Button>
